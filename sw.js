404a555a641aaf04039fb19076d108b38ce8bbd7
hassi1020-tech <hassi1020@gmail.com>
2026-07-27 10:35:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.map(key=>caches.delete(key))))
-      .then(()=>self.registration.unregister())
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener("fetch",()=>{});
+const CACHE="heat-check-v11-1100";const FILES=["./","./index.html","./styles.css?v=1100","./app.js?v=1100","./manifest.webmanifest?v=1100"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
